4c4da1925a7139e4eddd553b4e28610837bfbf3f
John Carver <29667140+JCppe@users.noreply.github.com>
2019-11-21 17:34:13 -0500

    all files initialized

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
+const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-var MONGODB_URI = process.env.MONGODB_URI || "mongo://localhost/mongoHeadlines";
+const db = require("./models")
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongo://localhost/mongoHeadlines";
+
+const PORT = 3000;
 
 mongoose.connect(MONGODB_URI);
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.json({});
+});
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
